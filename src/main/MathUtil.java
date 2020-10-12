b0d0087f8212401c444836321fc7303d8fa4f258
SayeedJ <sayeedjoseph@gmail.com>
2020-10-12 11:28:56 -0400

    first commit

---
+++ src/main/MathUtil.java
@@ -0,0 +1,16 @@
+package main;
+
+public class MathUtil {
+	
+	
+	public int add(int a, int b) {
+		return a + b; 
+	}
+	
+	public boolean isOdd(int a) {
+		if(a % 2 == 1) return true; 
+		
+		return false; 
+	}
+	
+}
